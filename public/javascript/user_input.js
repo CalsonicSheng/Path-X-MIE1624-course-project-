fae8378a5fba3d2ad6ba4f42c92ac89656046284
CalsonicSheng <calson.developer@gmail.com>
2021-11-29 21:24:27 -0700

    loading add

--- public/javascript/user_input.js
+++ public/javascript/user_input.js
@@ -1,31 +1,36 @@
-const allSkillElements = document.querySelectorAll(".grid-container p");
+const allSkillElements = document.querySelectorAll('.grid-container p');
 
-const skillInput = document.querySelector("#skills-tree");
+const skillInput = document.querySelector('#skills-tree');
 
-const careerPathInput = document.querySelector("#career-path");
+const careerPathInput = document.querySelector('#career-path');
 
-const submitButton = document.querySelector(".btn-submit-custom");
+const submitButton = document.querySelector('.btn-submit-custom');
 
-const emptyErrorMessage = document.querySelector(".empty-error-message");
+const emptyErrorMessage = document.querySelector('.empty-error-message');
 
 const skillInputList = [];
 
+const mainContent = document.querySelector('.main-content');
+const loaderWrapper = document.querySelector('.initial-loader-wrapper');
+const outerLoader = document.querySelector('.outer-loader');
+const innerLoader = document.querySelector('.inner-loader');
+
 // skill bubble chip event listener logic
 allSkillElements.forEach(function (e) {
-  e.addEventListener("click", function () {
+  e.addEventListener('click', function () {
     if (e.classList.length == 1) {
-      e.classList.remove("clicked");
+      e.classList.remove('clicked');
       const targetIndex = skillInputList.indexOf(e.textContent);
       skillInputList.splice(targetIndex, 1);
       if (skillInputList.length == 0) {
-        skillInput.value = "";
+        skillInput.value = '';
       } else {
-        skillInput.value = skillInputList.join(", ") + ", ";
+        skillInput.value = skillInputList.join(', ') + ', ';
         console.log(skillInputList);
         console.log(skillInput.value);
       }
     } else {
-      e.classList.add("clicked");
+      e.classList.add('clicked');
       skillInput.value += `${e.textContent}, `;
       skillInputList.push(e.textContent);
       console.log(skillInputList);
@@ -35,38 +40,18 @@ allSkillElements.forEach(function (e) {
 });
 
 // final submit button eventListener logic: prevent form-data submit if all required field are empty
-submitButton.addEventListener("click", function () {
+submitButton.addEventListener('click', function () {
   careerPathInput.value = careerPathInput.value.toLowerCase();
-  if (skillInput.value == "") {
-    submitButton.setAttribute("type", "button");
-    emptyErrorMessage.classList.add("show");
-    console.log("empty");
+  if (skillInput.value == '') {
+    submitButton.setAttribute('type', 'button');
+    emptyErrorMessage.classList.add('show');
+    console.log('empty');
   } else {
-    emptyErrorMessage.classList.remove("show");
-    submitButton.setAttribute("type", "submit");
+    emptyErrorMessage.classList.remove('show');
+    submitButton.setAttribute('type', 'submit');
+    mainContent.classList.add('.main-content-hidden');
+    loaderWrapper.classList.add('loader-wrapper');
+    outerLoader.classList.add('loader');
+    innerLoader.classList.add('loader-inner');
   }
 });
-
-// const initiallist = [];
-// initiallist.push(...["111", "222", "33"]);
-
-// console.log(initiallist);
-
-// console.log(initiallist.join(", "));
-
-// const targetString = "1, 2, ccc, cbbb, 3, 4, kkk, ";
-// const trims = targetString.slice(0, targetString.length - 2);
-
-// console.log(trims);
-
-// const targetStringList = trims.split(", ");
-
-// console.log(targetStringList);
-
-// const targetindex = targetStringList.indexOf("cbbb");
-
-// console.log(targetindex);
-
-// targetStringList.splice(targetindex, 1);
-
-// console.log(targetStringList);
